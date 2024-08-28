3aa62d853035a6c0016bd20d22cb2c25401de4b1
giadantioco <giada.antioco@gmail.com>
2024-08-28 14:29:27 +0200

    fix: jump logic

--- flappy-bird/app.js
+++ flappy-bird/app.js
@@ -7,6 +7,8 @@ document.addEventListener("DOMContentLoaded", () => {
   let birdBottom = 100;
   let gravity = 2;
 
+  // jump
+
   function startGame() {
     birdBottom -= gravity;
     bird.style.bottom = birdBottom + "px";
@@ -14,9 +16,29 @@ document.addEventListener("DOMContentLoaded", () => {
   }
   let timerID = setInterval(startGame, 20);
 
+  function control(e) {
+    if (e.keyCode === 32) {
+      // 32 = spacebar!
+      jump();
+    }
+  }
+
   function jump() {
     if (birdBottom < 500) birdBottom += 50;
     bird.style.bottom = birdBottom + "px";
   }
-  document.addEventListener("keyup", jump);
+  document.addEventListener("keyup", control);
+
+  // obstacles
+
+  function generateObstacle() {
+    let obstacleLeft = 500;
+    let obstacleBottom = 150;
+    const obstacle = document.createElement("div");
+    obstacle.classList.add("obstacle");
+    gameDisplay.appendChild(obstacle);
+    obstacle.style.left = obstacleLeft + "px";
+    obstacle.style.bottom = obstacleBottom + "px";
+  }
+  generateObstacle();
 });
